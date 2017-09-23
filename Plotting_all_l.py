304b6596fbaffa4ba8b80c0c7d2e7614323642b7
sandeep <sandeeprana@iisermohali.ac.in>
2017-09-24 03:18:40 +0530

    Changes made in NISER

--- Plotting_all_l.py
+++ Plotting_all_l.py
@@ -7,8 +7,8 @@ from matplotlib.colors import LogNorm
 lmax = 250
 
 
-s1 = '/dataspace/sandeep/Bispectrum_data/Gaussian_50K_test/'
-s2 = 'All_mode/Analysis_Bin_Bispectrum_128_50K.txt'
+s1 = '/dataspace/sandeep/Bispectrum_data/Gaussian_60K_test/'
+s2 = 'All_mode/Analysis_Bin_Bispectrum_128_60K.txt'
 
 
 name = s1+s2
@@ -58,15 +58,15 @@ else:
     I3 = np.asarray(k)
     I2 = np.asarray(j)
     I1 = np.asarray(i)
-
+    l3 = np.asarray(l3)
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 esti_bis = np.zeros((1000, len(Bis1)), dtype=np.float64)
 esti_bis_1 = np.zeros((1000, len(Bis2)), dtype=np.float64)
 
 
 for ii in xrange(0, 1000):
-    s1 = '/dataspace/sandeep/Bispectrum_data/Gaussian_50K_test/Gaussian_Bin_Bispectrum/'
-    s2 = 'BinnedBispectrum_Bin_GaussianMaps_linCorr_128_50K_%d.txt' % ii
+    s1 = '/dataspace/sandeep/Bispectrum_data/Gaussian_60K_test/Gaussian_Bin_Bispectrum/'
+    s2 = 'BinnedBispectrum_Bin_GaussianMaps_linCorr_128_60K_%d.txt' % ii
 
     name = s1+s2
     data = ascii.read(name, guess=False, delimiter='\t')
@@ -128,48 +128,56 @@ for i in xrange(0, nbin):
 cmap = plt.cm.RdBu
 cmap.set_bad(color='grey')
 
+file_name = "/dataspace/sandeep/Bispectrum_data/Gaussian_60K_test/plots/3sigma_table.txt"
+with open(file_name, 'w') as f:
+    f.write("delta\tI1\tI2\tI3\n")
 
-def plot_data(count1):
 
-    data = np.zeros((nbin-1, nbin-1), dtype=np.float64)
-    for ii in xrange(len(I3)):
-        if I3[ii] == count1:
-            index, index1 = I2[ii], I1[ii]
-    #        temp = (Bis1[ii]-mean[ii])/std_dev[ii]
-            #if -2.0 > temp or temp > 2.0:
-            data[index, index1] = (Bis1[ii]-mean[ii])/std_dev[ii]
+    def plot_data(count1):
+        data = np.zeros((nbin-1, nbin-1), dtype=np.float64)
+        for ii in xrange(len(I3)):
+            if I3[ii] == count1:
+                index, index1 = I1[ii], I2[ii]
+                data[index, index1] = (Bis1[ii]-mean[ii])/std_dev[ii]
+                if (Bis1[ii]-mean[ii])/std_dev[ii] >3.0:
+                    f.write("I3 Bin %d\n" % I3[ii])
+                    f.write("%0.6e\t%d\t%d\t%d\n" % ((Bis1[ii]-mean[ii])/std_dev[ii], I1[ii], I2[ii], I3[ii]))
+                    f.write("\n")
+
     #data = np.ma.masked_where(data == 0.0, data)
-    return data, [bin_arr[count1, 0], bin_arr[count1, 1]]
+
+        return data, [bin_arr[count1, 0], bin_arr[count1, 1]]
 
 
-Indx = 0
-Indxy = 0
-nn = 0
+    Indx = 0
+    Indxy = 0
+    nn = 0
 
-fig = plt.figure(1, figsize=(12, 15))
-gs = gridspec.GridSpec(4, 4, hspace=0.5, wspace=0.6)
-#gs.update(wspace=0.025, hspace=0.05) # set the spacing between axes.
-for Indx in xrange(0, 3):
-    for Indy in xrange(0, 3):
-        a, b = plot_data(nn)
-        ax1 = plt.subplot(gs[Indx, Indy])
-        im = ax1.imshow(a, cmap=cmap, origin='lower', interpolation='none')
-        ax1.set_xlabel(r'$I_{1}$', fontsize=14)
-        ax1.set_ylabel(r'$I_{2}$', fontsize=14)
-        ax1.set_title(r'$I_{3}\in [%d, %d]$' % (b[0], b[1]))
-        ax1.set_aspect('equal')
-        plt.colorbar(im,  spacing='proportional',  fraction=0.046, pad=0.04)
-        nn += 1
-a, b = plot_data(nn)
-ax1 = plt.subplot(gs[3, 0])
-im = ax1.imshow(a, cmap=cmap, origin='lower', interpolation='none')
-ax1.set_xlabel(r'$I_{1}$', fontsize=14)
-ax1.set_ylabel(r'$I_{2}$', fontsize=14)
-ax1.set_title(r'$I_{3}\in [%d, %d]$' % (b[0], b[1]))
-ax1.set_aspect('equal')
-plt.colorbar(im,  spacing='proportional',  fraction=0.046, pad=0.04)
-#plt.tight_layout()  # Or equivalently,  "plt.tight_layout()"
-plt.savefig("/dataspace/sandeep/Bispectrum_data/Gaussian_50K_test/plots/Allmode_2dplot_Bin_Bispectrum_nbin-11.png", dpi=600)
+    fig = plt.figure(1, figsize=(9, 10))
+    gs = gridspec.GridSpec(4, 3, left=0.05, hspace=0.5, wspace=0.4)
+
+    for Indx in xrange(0, 3):
+        for Indy in xrange(0, 3):
+            a, b = plot_data(nn)
+            ax1 = plt.subplot(gs[Indx, Indy])
+            im = ax1.imshow(a, cmap=cmap, origin='lower', interpolation='none')
+            ax1.set_xlabel(r'$I_{1}$', fontsize=14)
+            ax1.set_ylabel(r'$I_{2}$', fontsize=14)
+            ax1.set_title(r'$I_{3}\in [%d, %d]$' % (b[0], b[1]))
+            ax1.set_aspect('equal')
+            plt.colorbar(im,  spacing='proportional',  fraction=0.046, pad=0.04)
+            nn += 1
+    a, b = plot_data(nn)
+    ax1 = plt.subplot(gs[3, 0])
+    im = ax1.imshow(a, cmap=cmap, origin='lower', interpolation='none')
+    ax1.set_xlabel(r'$I_{1}$', fontsize=14)
+    ax1.set_ylabel(r'$I_{2}$', fontsize=14)
+    ax1.set_title(r'$I_{3}\in [%d, %d]$' % (b[0], b[1]))
+    ax1.set_aspect('equal')
+    plt.colorbar(im,  spacing='proportional',  fraction=0.046, pad=0.04)
+
+    plt.savefig("/dataspace/sandeep/Bispectrum_data/Gaussian_60K_test/plots/Allmode_2dplot_Bin_Bispectrum_nbin-11.png", dpi=600)
+#plt.savefig('/dataspace/sandeep/Bispectrum_data/test.png', dpi=600)
 
 
 plt.figure(3, figsize=(8, 6))
@@ -185,5 +193,5 @@ plt.xlabel(r"$I$", fontsize=18)
 plt.ylabel(r"$B_{III}$", fontsize=18)
 #plt.xlim(6,)
 plt.yscale('symlog', linthreshy=0.001)
-plt.savefig('/dataspace/sandeep/Bispectrum_data/Gaussian_50K_test/plots/Allmode_Bin_Bispectrum_lll_nbin-11.png', dpi=600)
+plt.savefig('/dataspace/sandeep/Bispectrum_data/Gaussian_60K_test/plots/Allmode_Bin_Bispectrum_lll_nbin-11.png', dpi=600)
 plt.show()
--- plot.py
+++ plot.py
@@ -19,47 +19,48 @@ haslam = Haslam_128 * mask_80K
 hp.mollview(haslam, xsize=2000, unit=r'$T_{B}(K)$', nest=False, title='%s' % '80K')
 name = '/dataspace/sandeep/Bispectrum_data/Input_Maps/maps_128/Map_80K_apod_300arcm_ns_128.png'
 plt.savefig(name, dpi=600)
-
+"""
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 name = '/dataspace/sandeep/Bispectrum_data/Input_Maps/mask_apod_128/Mask_60K_apod_300arcm_ns_128.fits'
 mask_60K = hp.fitsfunc.read_map(name, verbose=False)
-haslam = Haslam_128 * mask_60K
-
-hp.mollview(haslam, xsize=2000, unit=r'$T_{B}(K)$', nest=False, title='%s' % '60K')
-name = '/dataspace/sandeep/Bispectrum_data/Input_Maps/maps_128/Map_60K_apod_300arcm_ns_128.png'
-plt.savefig(name, dpi=600)
-
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+haslam_60K = Haslam_128 * mask_60K
 
 name = '/dataspace/sandeep/Bispectrum_data/Input_Maps/mask_apod_128/Mask_50K_apod_300arcm_ns_128.fits'
 mask_50K = hp.fitsfunc.read_map(name, verbose=False)
-haslam = Haslam_128 * mask_50K
-
-hp.mollview(haslam, xsize=2000, unit=r'$T_{B}(K)$', nest=False, title='%s' % '50K')
-name = '/dataspace/sandeep/Bispectrum_data/Input_Maps/maps_128/Map_50K_apod_300arcm_ns_128.png'
-plt.savefig(name, dpi=600)
-
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+haslam_50K = Haslam_128 * mask_50K
 
 name = '/dataspace/sandeep/Bispectrum_data/Input_Maps/mask_apod_128/Mask_40K_apod_300arcm_ns_128.fits'
 mask_40K = hp.fitsfunc.read_map(name, verbose=False)
-haslam = Haslam_128 * mask_40K
-
-hp.mollview(haslam, xsize=2000, unit=r'$T_{B}(K)$', nest=False, title='%s' % '40K')
-name = '/dataspace/sandeep/Bispectrum_data/Input_Maps/maps_128/Map_40K_apod_300arcm_ns_128.png'
-plt.savefig(name, dpi=600)
-
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+haslam_40K = Haslam_128 * mask_40K
 
 name = '/dataspace/sandeep/Bispectrum_data/Input_Maps/mask_apod_128/Mask_30K_apod_300arcm_ns_128.fits'
 mask_30K = hp.fitsfunc.read_map(name, verbose=False)
-haslam = Haslam_128 * mask_30K
+haslam_30K = Haslam_128 * mask_30K
 
-hp.mollview(haslam, xsize=2000, unit=r'$T_{B}(K)$', nest=False, title='%s' % '30K')
-name = '/dataspace/sandeep/Bispectrum_data/Input_Maps/maps_128/Map_30K_apod_300arcm_ns_128.png'
-plt.savefig(name, dpi=600)
-"""
+
+dpi = 600
+fig_size_inch = 12, 12
+fig = plt.figure(1, figsize=(12, 12))
+
+
+hp.mollview(haslam_60K,fig=fig.number, xsize=fig_size_inch[0]*dpi, unit=r'$T_{B}(K)$', nest=False, sub=(2, 2, 1), title='%s' % '60K')
+hp.mollview(haslam_50K,fig=fig.number, xsize=fig_size_inch[0]*dpi, unit=r'$T_{B}(K)$', nest=False, sub=(2, 2, 2), title='%s' % '50K')
+hp.mollview(haslam_40K,fig=fig.number, xsize=fig_size_inch[0]*dpi, unit=r'$T_{B}(K)$', nest=False, sub=(2, 2, 3), title='%s' % '40K')
+hp.mollview(haslam_30K,fig=fig.number, xsize=fig_size_inch[0]*dpi, unit=r'$T_{B}(K)$', nest=False, sub=(2, 2, 4), title='%s' % '30K')
+
+fig.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.05, hspace=0.05)
+name = '/dataspace/sandeep/Bispectrum_data/Map_all_apod_300arcm_ns_128.png'
+plt.savefig(name, dpi=600, bbox_inches="tight")
+
+
+
+
+
+
+
+
+
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 key = ['60K', '50K', '40K', '30K']
@@ -71,14 +72,15 @@ for i in xrange(len(key)):
     ap_map_128 = hp.fitsfunc.read_map(f_name1)
     sky_sum.append(np.sum(ap_map_128))
 
-#nbin = 11
-#index = np.logspace(np.log10(10), np.log10(256), 11, endpoint=True, dtype=np.int32)
+nbin = 11
+index = np.logspace(np.log10(10), np.log10(256), 11, endpoint=True, dtype=np.int32)
+
 # *****************************************************************
 # for 15 bin scheme
 
-index = [10, 19, 27, 39, 46, 55, 65, 77, 91, 109, 129, 153, 181, 215, 256]
-index = np.asarray(index, dtype=np.int32)
-nbin = len(index)
+#index = [10, 19, 27, 39, 46, 55, 65, 77, 91, 109, 129, 153, 181, 215, 256]
+#index = np.asarray(index, dtype=np.int32)
+#nbin = len(index)
 
 print nbin
 bin_arr = np.zeros((nbin - 1, 2), dtype=np.int32)
@@ -91,21 +93,23 @@ for i in xrange(0, nbin-1):
     ini = index[i]
     if i+1 < nbin:
         final = index[i+1]
+        """
         if ini+5 > final:
             bin_arr[i, 0] = ini
             temp = abs(final-ini)
             bin_arr[i, 1] = final+temp
             index[i+1] = final+temp
         else:
-            bin_arr[i, 0] = ini
-            bin_arr[i, 1] = final
+        """
+        bin_arr[i, 0] = ini
+        bin_arr[i, 1] = final
 
 
 Indx = 0
 Indy = 0
-Actual = np.zeros((5, nbin-1), dtype=np.float32)
-Mean = np.zeros((5, nbin-1), dtype=np.float32)
-Std_Dev = np.zeros((5, nbin-1), dtype=np.float32)
+Actual = np.zeros((5, nbin-1), dtype=np.float64)
+Mean = np.zeros((5, nbin-1), dtype=np.float64)
+Std_Dev = np.zeros((5, nbin-1), dtype=np.float64)
 
 lmax = 3*128-1
 
@@ -131,14 +135,16 @@ for fn in key:
             s2 = "/Gaussian_%s_test/Gaussian_%s_cl/haslam_%sgaussMap_cl_%d.txt" % (fn, fn, fn, nn)
             filename = s1+s2
             Map_cl = np.genfromtxt(filename)
-            Sum.append(np.sum(Map_cl*window_func*l*(l+1))/length)
+            #Sum.append(np.sum(Map_cl*window_func*l*(l+1))/length)
+
+            Sum.append(np.sum(np.multiply(Map_cl*l*(l+1), window_func))/length)
 
         Sum = np.asarray(Sum)
-        Actual[count, i] = np.sum(cl*window_func*l*(l+1))/length
+        #Actual[count, i] = np.sum(cl*window_func*l*(l+1))/length
+        Actual[count, i] = np.sum(np.multiply(cl*l*(l+1), window_func))/length
         Mean[count, i] = np.mean(Sum)
         Std_Dev[count, i] = np.std(Sum)
     count += 1
-
 
 #bl = hp.sphtfunc.gauss_beam(np.radians(33./60.), lmax=3*128-1)
 #wl = hp.sphtfunc.pixwin(128, pol=False)
@@ -162,13 +168,11 @@ ax1.errorbar(I, Mean[0, :], yerr=Std_Dev[0, :], fmt='ko', ecolor='k', capthick=4
 #        label='beam_window')
 
 ax1.set_yscale("log")
-#plt.grid(which='both')
-plt.legend(loc=3)
 plt.ylabel(r'$C_{I}$', fontsize='large', fontstyle='italic', weight='extra bold')
 plt.minorticks_on()
 plt.tick_params(axis='both', which='minor', length=5, width=2, labelsize=10)
 plt.tick_params(axis='both', which='major', length=8, width=2, labelsize=10)
-plt.xlim(0.9, 15)
+plt.xlim(0.0, 11)
 ax1.text(4, 450., r'$\mathbf{T_{Cut}:}$ 60K, '
                  r'$\mathbf{f_{sky}:}$ '
                  ' %0.1f %%' % (frac_sky[0]*100),
@@ -178,14 +182,12 @@ ax1.text(4, 450., r'$\mathbf{T_{Cut}:}$ 60K, '
 ax2 = plt.subplot(gs[1, 0], sharex=ax1)
 ax2.plot(I, (Actual[0, :]-Mean[0, :])/Std_Dev[0, :], 'bo-', linewidth=2)
 ax2.axhline(y=0.0, color='k', linewidth=2)
-#plt.grid(which='both')
-plt.legend(loc=3)
 plt.xlabel(r'$I$', fontsize='large', fontstyle='italic', weight='extra bold')
 plt.ylabel(r'$\Delta$', fontsize='large', fontstyle='italic', weight='extra bold')
 plt.minorticks_on()
 plt.tick_params(axis='both', which='minor', length=5, width=2, labelsize=10)
 plt.tick_params(axis='both', which='major', length=8, width=2, labelsize=10)
-plt.xlim(0.9, 15)
+plt.xlim(0.0, 11)
 plt.ylim(-1.0, 1.0)
 
 
@@ -193,33 +195,30 @@ ax3 = plt.subplot(gs[0, 1])
 ax3.plot(I, Actual[1, :], '-', ms=5,  color='b', linewidth=2)
 ax3.errorbar(I, Mean[1, :], yerr=Std_Dev[1, :], fmt='ko', ecolor='k', capthick=4)
 ax3.set_yscale("log")
-#plt.grid(which='both')
-plt.legend(loc=3)
 plt.ylabel(r'$C_{I}$', fontsize='large', fontstyle='italic', weight='extra bold')
 plt.minorticks_on()
 plt.tick_params(axis='both', which='minor', length=5, width=2, labelsize=10)
 plt.tick_params(axis='both', which='major', length=8, width=2, labelsize=10)
-plt.xlim(0.9, 15)
+plt.xlim(0.0, 11)
 ax3.text(4.0, 450, r'$\mathbf{T_{Cut}:}$ 50K, '
-                 r'$\mathbf{f_{sky}:}$ '
-                 ' %0.1f %%' % (frac_sky[1]*100),
-                 bbox={'facecolor':'white', 'alpha': 0.5, 'pad': 4})#, verticalalignment='bottom')
+                   r'$\mathbf{f_{sky}:}$ '
+                   ' %0.1f %%' % (frac_sky[1]*100),
+                   bbox={'facecolor': 'white', 'alpha': 0.5, 'pad': 4})  #, verticalalignment='bottom')
 
 ax4 = plt.subplot(gs[1, 1], sharex=ax3)
 ax4.plot(I, (Actual[1, :]-Mean[1, :])/Std_Dev[1, :], 'bo-', linewidth=2)
 ax4.axhline(y=0.0, color='k', linewidth=2)
-#plt.grid(which='both')
-plt.legend(loc=3)
 plt.xlabel(r'$I$', fontsize='large', fontstyle='italic', weight='extra bold')
 plt.ylabel(r'$\Delta$', fontsize='large', fontstyle='italic', weight='extra bold')
 plt.minorticks_on()
 plt.tick_params(axis='both', which='minor', length=5, width=2, labelsize=10)
 plt.tick_params(axis='both', which='major', length=8, width=2, labelsize=10)
-plt.xlim(0.9, 15)
+plt.xlim(0.0, 11)
 plt.ylim(-1.0, 1.0)
 
-fig.savefig("/dataspace/sandeep/Bispectrum_data/BinnedCl_1000sim_1_nbin-15.png", dpi=600)
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++=
+#fig.savefig("/dataspace/sandeep/Bispectrum_data/BinnedCl_1000sim_1_nbin-15.png", dpi=600)
+fig.savefig("/dataspace/sandeep/Bispectrum_data/BinnedCl_1000sim_1.png", dpi=600)
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 fig = plt.figure(7, figsize=(10, 8))
 gs = gridspec.GridSpec(2, 2, height_ratios=[2, 1], hspace=0.01, wspace=0.3)
@@ -228,29 +227,25 @@ ax5 = plt.subplot(gs[0, 0])
 ax5.plot(I, Actual[2, :], '-', ms=5,  color='b', linewidth=2)
 ax5.errorbar(I, Mean[2, :], yerr=Std_Dev[2, :], fmt='ko', ecolor='k', capthick=4)
 ax5.set_yscale("log")
-#plt.grid(which='both')
-plt.legend(loc=3)
 plt.ylabel(r'$C_{I}$', fontsize='large', fontstyle='italic', weight='extra bold')
 plt.minorticks_on()
 plt.tick_params(axis='both', which='minor', length=5, width=2, labelsize=10)
 plt.tick_params(axis='both', which='major', length=8, width=2, labelsize=10)
-plt.xlim(0.9, 15)
+plt.xlim(0., 11)
 ax5.text(4.0, 450, r'$\mathbf{T_{Cut}:}$ 40K, '
                  r'$\mathbf{f_{sky}:}$ '
                  ' %0.1f %%' % (frac_sky[2]*100),
                  bbox={'facecolor':'white', 'alpha': 0.5, 'pad': 5})#, verticalalignment='bottom')
 
 ax6 = plt.subplot(gs[1, 0], sharex=ax5)
-ax6.plot(I, (Actual[2, :]-Mean[2, :])/Std_Dev[1, :], 'bo-', linewidth=2)
+ax6.plot(I, (Actual[2, :]-Mean[2, :])/Std_Dev[2, :], 'bo-', linewidth=2)
 ax6.axhline(y=0.0, color='k', linewidth=2)
-#plt.grid(which='both')
-plt.legend(loc=3)
 plt.xlabel(r'$I$', fontsize='large', fontstyle='italic', weight='extra bold')
 plt.ylabel(r'$\Delta$', fontsize='large', fontstyle='italic', weight='extra bold')
 plt.minorticks_on()
 plt.tick_params(axis='both', which='minor', length=5, width=2, labelsize=10)
 plt.tick_params(axis='both', which='major', length=8, width=2, labelsize=10)
-plt.xlim(0.9, 15)
+plt.xlim(0., 11)
 plt.ylim(-1.0, 1.0)
 
 
@@ -258,13 +253,11 @@ ax7 = plt.subplot(gs[0, 1])
 ax7.plot(I, Actual[3, :], '-', ms=5,  color='b', linewidth=2)
 ax7.errorbar(I, Mean[3, :], yerr=Std_Dev[3, :], fmt='ko', ecolor='k', capthick=4)
 ax7.set_yscale("log")
-#plt.grid(which='both')
-plt.legend(loc=3)
 plt.ylabel(r'$C_{I}$', fontsize='large', fontstyle='italic', weight='extra bold')
 plt.minorticks_on()
 plt.tick_params(axis='both', which='minor', length=5, width=2, labelsize=10)
 plt.tick_params(axis='both', which='major', length=8, width=2, labelsize=10)
-plt.xlim(0.9, 15)
+plt.xlim(0., 11)
 ax7.text(4.0, 450.0, r'$\mathbf{T_{Cut}:}$ 30K, '
                  r'$\mathbf{f_{sky}:}$ '
                  ' %0.1f %%' % (frac_sky[3]*100),
@@ -272,19 +265,19 @@ ax7.text(4.0, 450.0, r'$\mathbf{T_{Cut}:}$ 30K, '
 
 
 ax8 = plt.subplot(gs[1, 1], sharex=ax7)
-ax8.plot(I, (Actual[2, :]-Mean[2, :])/Std_Dev[1, :], 'bo-', linewidth=2)
+ax8.plot(I, (Actual[3, :]-Mean[3, :])/Std_Dev[3, :], 'bo-', linewidth=2)
+
 ax8.axhline(y=0.0, color='k', linewidth=2)
-#plt.grid(which='both')
-plt.legend(loc=3)
 plt.xlabel(r'$I$', fontsize='large', fontstyle='italic', weight='extra bold')
 plt.ylabel(r'$\Delta$', fontsize='large', fontstyle='italic', weight='extra bold')
 plt.minorticks_on()
 plt.tick_params(axis='both', which='minor', length=5, width=2, labelsize=10)
 plt.tick_params(axis='both', which='major', length=8, width=2, labelsize=10)
-plt.xlim(0.9, 15)
+plt.xlim(0., 11)
 plt.ylim(-1.0, 1.0)
 
-fig.savefig("/dataspace/sandeep/Bispectrum_data/BinnedCl_1000sim_2_nbin-15.png", dpi=600)
+#fig.savefig("/dataspace/sandeep/Bispectrum_data/BinnedCl_1000sim_2_nbin-15.png", dpi=600)
+fig.savefig("/dataspace/sandeep/Bispectrum_data/BinnedCl_1000sim_2.png", dpi=600)
 
 """
 ax1 = plt.subplot(gs[0, 1])
